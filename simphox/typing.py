c3410a35ad4182067439adeebb116e69a099c23e
Sunil Pai <sunilpai@stanford.edu>
2019-12-16 11:51:58 -0500

    finished some cosmetic changes to component

--- simphox/typing.py
+++ simphox/typing.py
@@ -1,8 +1,9 @@
-from typing import Union, Tuple, List, Optional
+from typing import Union, Tuple, List, Optional, Dict
 
 Shape2 = Tuple[int, int]
 Shape3 = Tuple[int, int, int]
 Dim2 = Tuple[float, float]
 Dim3 = Tuple[float, float, float]
+Dim4 = Tuple[float, float, float, float]
 GridShape = Union[Shape2, Shape3]
 GridSpacing = Union[float, Tuple[float, float, float]]
