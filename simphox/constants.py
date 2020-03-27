c16ada5836b387b3db96b466b4e1157dc9a7c1ea
sunilkpai <sunilpai@stanford.edu>
2020-03-27 12:04:25 -0700

    improved the nazca module, added some more functionality, splitter tree still needs more testing

--- simphox/constants.py
+++ simphox/constants.py
@@ -5,19 +5,70 @@ MU_0 = 1.25663706e-6             # vacuum permeability
 C_0 = 1 / np.sqrt(EPS_0 * MU_0)  # speed of light in vacuum
 ETA_0 = np.sqrt(MU_0 / EPS_0)    # vacuum impedance
 
-# nd.add_layer(name='Wvg',  layer=10, accuracy=0.001)
-# nd.add_layer(name='Grat', layer=11, accuracy=0.001)
-# nd.add_layer(name='Slab', layer=12, accuracy=0.001)
-# nd.add_layer(name='PIM', layer=23, accuracy=0.001)
-# nd.add_layer(name='NIM', layer=24, accuracy=0.001)
-# nd.add_layer(name='Excl', layer=90, accuracy=0.001)
-# nd.add_layer(name='Via1', layer=100, accuracy=0.001)
-# nd.add_layer(name='Mt1', layer=105, accuracy=0.001)
-# nd.add_layer(name='Htr', layer=115, accuracy=0.001)
-# nd.add_layer(name='Via2', layer=120, accuracy=0.001)
-# nd.add_layer(name='Mt2', layer=125, accuracy=0.001)
-# nd.add_layer(name='Pad', layer=150, accuracy=0.001)
-# nd.add_layer(name='DT', layer=160, accuracy=0.001)
-# nd.add_layer(name='LBL', layer=80, accuracy=0.001)
-# nd.add_layer(name='BND', layer=82, accuracy=0.001)
-# nd.add_layer(name='DRCExcl', layer=1000, accuracy=0.001)
+AMF_STACK = {
+    'layers': {
+        'waveguide': 10,
+        'grating': 11,
+        'via': 100,
+        'via_heater': 120,
+        'mt_heater': 125,
+        'heater': 115,
+        'slab': 12,
+        'pad': 150,
+        'trench': 160
+    },
+    'cross_sections': {
+        'heater_xs': [
+            {
+                'layer': 115,  # heater
+                'growx': 0.755,  # (waveguide_w - heater_w) / 2 + 0.005
+                'growy': 0.005
+            },
+            {
+                'layer': 10,  # waveguide
+                'growy': 0.001
+            }
+        ],
+        'via_heater_xs': [
+            {
+                'layer': 120  # via_heater
+            },
+            {
+                'layer': 125,  # mt_heater
+                'growx': 1.5,
+                'growy': 1.5
+            }
+        ],
+        'grating_xs': [
+            {
+                'layer': 11  # grating
+            }
+        ],
+        'waveguide_xs': [
+            {
+                'layer': 10,  # waveguide
+                'growy': 0.004
+            }
+        ],
+        'pad_xs': [
+            {
+                'layer': 125  # mt_heater
+            },
+            {
+                'layer': 150,  # pad
+                'growx': -2,
+                'growy': -2
+            }
+        ],
+        'trench_xs': [
+            {
+                'layer': 160  # trench
+            }
+        ],
+        'slab_xs': [
+            {
+                'layer': 12  # slab
+            }
+        ]
+    }
+}
