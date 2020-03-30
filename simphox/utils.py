c781671c17785c11a02442f185b7473ba7f5a269
sunilkpai <sunilpai@stanford.edu>
2020-03-29 18:54:08 -0700

    some renaming, added coupler path for more granular component designs

--- simphox/utils.py
+++ simphox/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse as sp
+import matplotlib.colors as mcolors
 
 from .typing import List, Callable, Optional
 
@@ -29,18 +30,23 @@ def grid_average(params: np.ndarray) -> np.ndarray:
     return np.stack([p_x, p_y, p_z])
 
 
-def emplot(ax, val, eps, spacing: Optional[float] = None, field_cmap: str = 'RdBu', alpha=0.9):
+def emplot(ax, val, eps, spacing: Optional[float] = None, field_cmap: str = 'RdBu', alpha=0.9, div_norm=False):
     nx, ny = val.shape
     extent = (0, int(nx * spacing), 0, int(ny * spacing)) if spacing else (0, nx, 0, ny)
     ax.imshow(eps.T, cmap='gray', origin='lower left', alpha=1, extent=extent)
-    ax.imshow(val.T, cmap=field_cmap, origin='lower left', alpha=alpha, extent=extent)
+    if div_norm:
+        im_val = val * np.sign(val.flat[np.abs(val).argmax()])
+        norm = mcolors.DivergingNorm(vcenter=0, vmin=-im_val.max(), vmax=im_val.max())
+        ax.imshow(im_val.T, cmap=field_cmap, origin='lower left', alpha=alpha, extent=extent, norm=norm)
+    else:
+        ax.imshow(val.T, cmap=field_cmap, origin='lower left', alpha=alpha, extent=extent)
     if spacing:  # in microns!
         ax.set_ylabel(r'$y$ ($\mu$m)')
         ax.set_xlabel(r'$x$ ($\mu$m)')
 
 
-def field_emplot_re(ax, field: np.ndarray, eps: np.ndarray, spacing: Optional[float] = None):
-    emplot(ax, field.real, eps, spacing)
+def field_emplot_re(ax, field: np.ndarray, eps: np.ndarray, spacing: Optional[float] = None, div_norm: bool = True):
+    emplot(ax, field.real, eps, spacing, div_norm=div_norm)
 
 
 def field_emplot_mag(ax, field: np.ndarray, eps: np.ndarray, spacing: Optional[float] = None):
