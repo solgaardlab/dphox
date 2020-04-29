adding fabrication, fixing hz computation (no more edge effects!), adding some overlap stuff (no testing yet)

simphox/simulation/fdfd.py
@@ -190,7 +190,7 @@ class FDFD(SimGrid):
         return self.reshape(e) if reshaped else e
 
     def wgm_solve(self, num_modes: int = 6, beta_guess: Optional[Union[float, Tuple[float, float]]] = None,
-                  tol: float = 1e-5) -> Tuple[np.ndarray, np.ndarray]:
+                  tol: float = 1e-7) -> Tuple[np.ndarray, np.ndarray]:
         """FDFD waveguide mode (WGM) solver
 
         Solve for waveguide modes (x-translational symmetry) by finding the eigenvalues of :math:`C`.
@@ -211,7 +211,8 @@ class FDFD(SimGrid):
 
         """
 
-        db = self.db
+        # db = self.db
+        df = self.df
         if isinstance(beta_guess, float) or beta_guess is None:
             sigma = beta_guess ** 2 if beta_guess else (self.k0 * np.sqrt(np.max(self.eps))) ** 2
             eigvals, eigvecs = eigs(self.wgm, k=num_modes, sigma=sigma, tol=tol)
@@ -222,7 +223,7 @@ class FDFD(SimGrid):
             raise TypeError(f'Expected beta_guess to be None, float, or Tuple[float, float] but got {type(beta_guess)}')
         inds_sorted = np.asarray(np.argsort(np.sqrt(eigvals.real))[::-1])
         if self.ndim > 1:
-            hz = sp.hstack(db[:2]) @ eigvecs / (1j * np.sqrt(eigvals))
+            hz = sp.hstack(df[:2]) @ eigvecs / (1j * np.sqrt(eigvals))
             h = np.vstack((eigvecs, hz))
         else:
             h = eigvecs

simphox/utils.py
@@ -13,6 +13,11 @@ def poynting_z(e: np.ndarray, h: np.ndarray):
     return e_cross[0] * h_cross.conj()[1] - e_cross[1] * h_cross.conj()[0]
 
 
+def overlap(e1: np.ndarray, h1: np.ndarray, e2: np.ndarray, h2: np.ndarray):
+    return (np.sum(poynting_z(e1, h2)) * np.sum(poynting_z(e2, h1)) /
+            np.sum(poynting_z(e1, h1))).real / np.sum(poynting_z(e2, h2)).real
+
+
 def d2curl_op(d: List[sp.spmatrix]) -> sp.spmatrix:
     o = sp.csr_matrix((d[0].shape[0], d[0].shape[0]))
     return sp.bmat([[o, -d[2], d[1]],
@@ -22,8 +27,12 @@ def d2curl_op(d: List[sp.spmatrix]) -> sp.spmatrix:
 
 def d2curl_fn(f: np.ndarray, df: Callable[[np.ndarray, int], np.ndarray], beta: float = None):
     if beta is not None:
-        return np.stack([df(f[2], 1) + 1j * beta * f[1], -1j * beta * f[0] - df(f[2], 0), df(f[1], 0) - df(f[0], 1)])
-    return np.stack([df(f[2], 1) - df(f[1], 2), df(f[0], 2) - df(f[2], 0), df(f[1], 0) - df(f[0], 1)])
+        return np.stack([df(f[2], 1) + 1j * beta * f[1],
+                         -1j * beta * f[0] - df(f[2], 0),
+                         df(f[1], 0) - df(f[0], 1)])
+    return np.stack([df(f[2], 1) - df(f[1], 2),
+                     df(f[0], 2) - df(f[2], 0),
+                     df(f[1], 0) - df(f[0], 1)])
 
 
 def grid_average(params: np.ndarray, shift: int = 1) -> np.ndarray: