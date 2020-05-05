f4c57f556fbe788bfbf22fd746367b32de460dfe
nsabebe <nsabebe@LAPTOP-S4IAI6FR.localdomain>
2020-05-04 18:06:17 -0700

    merging all scripts to create master gds so far NR

--- simphox/design/aim.py
+++ simphox/design/aim.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr 23 17:19:16 2020
-
-@author: YuMiao
+@author: Yu Miao, Sunil Pai, Nate Abebe, Rebecca Hwang
 """
 
 import nazca as nd
@@ -37,6 +36,22 @@ class AIMPhotonicChip:
         nd.add_layer(name='WGKOAM', layer=(802, 727), overwrite=True, accuracy=accuracy)
         nd.add_layer(name='METKOAM', layer=(803, 727), overwrite=True, accuracy=accuracy)
         nd.add_layer(name='ABSTRACTAM', layer=(804, 727), overwrite=True, accuracy=accuracy)
+
+        nd.add_xsection('xs_si')
+        nd.add_layer2xsection(xsection='xs_si', layer='SEAM')
+        nd.add_xsection('xs_si_rib')
+        nd.add_layer2xsection(xsection='xs_si_rib', layer='SEAM')
+        nd.add_layer2xsection(xsection='xs_si_rib', layer='REAM', growx=0.25)
+        nd.add_xsection('xs_FN')
+        nd.add_layer2xsection(xsection='xs_FN', layer='FNAM')
+        nd.add_xsection('xs_sin')
+        nd.add_layer2xsection(xsection='xs_sin', layer='FNAM')
+        nd.add_xsection('xs_FNSN')
+        nd.add_layer2xsection(xsection='xs_FNSN', layer='FNAM')
+        nd.add_layer2xsection(xsection='xs_FNSN', layer='SNAM')
+        nd.add_xsection('xs_trench')
+        nd.add_layer2xsection(xsection='xs_trench', layer='SNAM')
+        nd.add_layer2xsection(xsection='xs_trench', layer='TZAM', growx=3, growy=3)
 
         self._pdk()
 
@@ -150,42 +165,78 @@ class AIMPhotonicChip:
             self.passive['cl_band_1p_tap_si'].pin['b1'] = nd.Pin('b1').put(40, -5, 0)
 
     # parameterized single mode waveguide for silicon and nitride
-    # 6a. silicon single mode waveguide
-    def cl_band_waveguide_si(self, length: float = 30, angle: float = 0, radius: Optional[float] = None):
-        """Create a length parameterized silicon SM WG
-
+    # 6z. silicon single mode rib waveguide
+    def cl_band_rib_waveguide_si(self, length: float = 30, angle: float = 0, radius: Optional[float] = None,
+                                 width: Optional[float] = None):
+        """Create a length parameterized silicon SM RWG
         Args:
             length (float): length of the SM WG
+        Returns:
+            Cell: SM RWG element
+        Note: PDK specify radius should be larger than 5um
+            We choose:
+                radius = 10um
+                width = 0.48um
+        """
+        with nd.Cell(name='nazca_cl_band_rib_waveguide_si') as cl_band_rib_waveguide_si:
+            ic = nd.interconnects.Interconnect(xs='xs_si_rib', radius=10 if radius is None else radius,
+                                               width=0.75 if not width else width)  # width of the hat
+            i1 = ic.bend(angle=angle, arrow=False).put() if angle != 0 else ic.strt(length=length, arrow=False).put()
+            # add pin
+            nd.Pin('a0', pin=i1.pin['a0']).put()
+            nd.Pin('b0', pin=i1.pin['b0']).put()
+        return cl_band_rib_waveguide_si
 
+    # 6a. silicon single mode waveguide
+    def cl_band_waveguide_si(self, length: float = 30, angle: float = 0, radius: Optional[float] = None,
+                             width: Optional[float] = None):
+        """Create a length parameterized silicon SM WG
+        Args:
+            length (float): length of the SM WG
         Returns:
             Cell: SM WG element
-
         Note: PDK specify radius should be larger than 5um
             We choose:
                 radius = 10um
                 width = 0.48um
         """
         with nd.Cell(name='nazca_cl_band_waveguide_si') as cl_band_waveguide_si:
-            nd.add_xsection('xs_si')
-            nd.add_layer2xsection(xsection='xs_si', layer='SEAM')
             ic = nd.interconnects.Interconnect(xs='xs_si', radius=10 if radius is None else radius,
-                                               width=self.waveguide_w)
+                                               width=self.waveguide_w if not width else width)
             i1 = ic.bend(angle=angle, arrow=False).put() if angle != 0 else ic.strt(length=length, arrow=False).put()
             # add pin
             nd.Pin('a0', pin=i1.pin['a0']).put()
             nd.Pin('b0', pin=i1.pin['b0']).put()
         return cl_band_waveguide_si
 
+    def cl_band_waveguide_si_thick(self, length: float = 30, angle: float = 0, radius: Optional[float] = None,
+                                   waveguide: float = 0.75):
+        """Create a length parameterized silicon SM WG
+        Args:
+            length (float): length of the SM WG
+        Returns:
+            Cell: SM WG element
+        Note: PDK specify radius should be larger than 5um
+            We choose:
+                radius = 10um
+                width = 0.48um
+        """
+        with nd.Cell(name='nazca_cl_band_waveguide_si_thick') as cl_band_waveguide_si_thick:
+            ic = nd.interconnects.Interconnect(xs='xs_si', radius=10 if radius is None else radius,
+                                               width=waveguide)
+            i1 = ic.bend(angle=angle, arrow=False).put() if angle != 0 else ic.strt(length=length, arrow=False).put()
+            # add pin
+            nd.Pin('a0', pin=i1.pin['a0']).put()
+            nd.Pin('b0', pin=i1.pin['b0']).put()
+        return cl_band_waveguide_si_thick
+
     # 6b. first layer nitride single mode waveguide
     def cl_band_waveguide_FN(self, length: float = 30, angle: float = 0, radius: Optional[float] = None):
         """Create a length parameterized silicon SM WG
-
         Args:
             length (float): length of the SM WG
-
         Returns:
             Cell: SM WG element
-
         Note: PDK specify radius should be larger than 100um
             We choose:
                 radius = 100um
@@ -193,8 +244,6 @@ class AIMPhotonicChip:
         """
 
         with nd.Cell(name='nazca_cl_band_waveguide_FN') as cl_band_waveguide_FN:
-            nd.add_xsection('xs_FN')
-            nd.add_layer2xsection(xsection='xs_FN', layer='FNAM')
             ic = nd.interconnects.Interconnect(xs='xs_FN', radius=100 if radius is None else radius, width=1.5)
             i1 = ic.bend(angle=angle, arrow=False).put() if angle != 0 else ic.strt(length=length, arrow=False).put()
 
@@ -207,13 +256,10 @@ class AIMPhotonicChip:
     def cl_band_waveguide_FNSN(self, length: float = 30, turn: bool = False, angle: float = 90,
                                radius: Optional[float] = None):
         """Create a length parameterized silicon SM WG
-
         Args:
             length: length of the SM WG
-
         Returns:
             Cell: SM WG element
-
         Note: PDK specify radius should be larger than 100um
             We choose:
                 radius = 40um
@@ -221,9 +267,6 @@ class AIMPhotonicChip:
         """
 
         with nd.Cell(name='nazca_cl_band_waveguide_FNSN') as cl_band_waveguide_FNSN:
-            nd.add_xsection('xs_FNSN')
-            nd.add_layer2xsection(xsection='xs_FNSN', layer='FNAM')
-            nd.add_layer2xsection(xsection='xs_FNSN', layer='SNAM')
             ic = nd.interconnects.Interconnect(xs='xs1', radius=40 if radius is None else radius, width=1.1)
             i1 = ic.bend(angle=angle, arrow=False).put() if turn else ic.strt(length=length, arrow=False).put()
             # add pin
@@ -265,10 +308,43 @@ class AIMPhotonicChip:
 
         return taper
 
+    def shallow_trench(self, length: float, width: float):
+        with nd.Cell(name='shallow_trench') as shallow_trench:
+            ic = nd.interconnects.Interconnect(xs='xs_trench')
+            ic.strt(length=length, width=width).put(0, 0)
+        return shallow_trench
+
+    def horntaper_si_ream(self, start_width, end_width, wavelength, n=500, name=None, xya=None):
+        with nd.Cell(name=f"{'cl_band_horntaper_si_ream' if name is None else name}") as taperREAM:
+            if start_width < end_width:
+                small_width, large_width = start_width, end_width
+            else:
+                small_width, large_width = end_width, start_width
+
+            xpoints1 = np.linspace(-0.5 * large_width, -0.5 * small_width, n // 2)
+            xpoints2 = np.linspace(0.5 * small_width, 0.5 * large_width, n // 2)
+            xpoints = np.concatenate((xpoints1, xpoints2))
+
+            ypoints = (4 * xpoints ** 2 - end_width ** 2) / (2 * wavelength)
+
+            taper_points = list(map(lambda x, y: (x, y), xpoints, ypoints))
+
+            nd.Polygon(points=taper_points, layer='REAM').put(*((0, 0, 0) if xya is None else xya))
+
+            if start_width < end_width:
+                nd.Pin('a0').put(0, -np.abs(ypoints).max(), -90)
+                nd.Pin('b0').put(0, 0, 90)
+
+            else:
+                nd.Pin('b0').put(0, 0, -90)
+                nd.Pin('a0').put(0, ypoints.max(), 90)
+
+            nd.put_stub()
+
+        return taperREAM
+
     def dc(self, gap_w: float, interaction_l: float, interport_w: float, end_l: float, radius: float):
         with nd.Cell(name='dc') as dc:
-            nd.add_xsection('xs_si')
-            nd.add_layer2xsection(xsection='xs_si', layer='SEAM')
             ic = nd.interconnects.Interconnect(xs='xs_si', radius=radius,
                                                width=self.waveguide_w)
             angle = self._mzi_angle(gap_w, interport_w, radius)
@@ -290,10 +366,34 @@ class AIMPhotonicChip:
 
         return dc
 
+        ### dc_rib, dc with rib waveguide
+
+    def dc_rib(self, gap_w: float, interaction_l: float, interport_w: float, end_l: float, radius: float,
+               waveguide: float):
+        with nd.Cell(name='dc_rib') as dc_rib:
+            ic = nd.interconnects.Interconnect(xs='xs_si_rib', radius=radius,
+                                               width=waveguide)
+            angle = self._mzi_angle(gap_w, interport_w, radius)
+            # upper path
+            nd.Pin('a0').put(0, 0, -180)
+            ic.strt(length=end_l).put(0, 0, 0)
+            _, iw, _ = coupler_path(ic, angle, interaction_l, radius)
+            nd.Pin('c0').put(iw.pin['a0'])
+            ic.strt(length=end_l).put()
+            nd.Pin('b0').put()
+
+            # lower path
+            nd.Pin('a1').put(0, interport_w, -180)
+            ic.strt(length=end_l).put(0, interport_w, 0)
+            _, iw, _ = coupler_path(ic, -angle, interaction_l, radius)
+            nd.Pin('c1').put(iw.pin['a0'])
+            ic.strt(length=end_l).put()
+            nd.Pin('b1').put()
+
+        return dc_rib
+
     def mzi(self, gap_w: float, interaction_l: float, interport_w: float, arm_l: float, end_l: float, radius: float):
         with nd.Cell(name='mzi') as mzi:
-            nd.add_xsection('xs_si')
-            nd.add_layer2xsection(xsection='xs_si', layer='SEAM')
             ic = nd.interconnects.Interconnect(xs='xs_si', radius=radius,
                                                width=self.waveguide_w)
             angle = self._mzi_angle(gap_w, interport_w, radius)
@@ -322,10 +422,9 @@ class AIMPhotonicChip:
 
     def microbridge_ps(self, bridge_w: float, bridge_l: float, tether_w: float,
                        tether_l: float, block_w: float, block_l: float,
-                       radius: float = 10, ring_shape: bool = True):
+                       radius: float = 10, ring_shape: bool = True,
+                       etch_hole: bool = True):
         with nd.Cell(name='microbridge_ps') as microbridge_ps:
-            nd.add_xsection('xs_sin')
-            nd.add_layer2xsection(xsection='xs_sin', layer='FNAM')
             ic = nd.interconnects.Interconnect(xs='xs_sin', radius=radius,
                                                width=block_w)
 
@@ -339,6 +438,7 @@ class AIMPhotonicChip:
 
             tether_points = geom.box(length=tether_l, width=tether_w)
             bridge_points = geom.box(length=bridge_l, width=bridge_w)
+            vert_bridge_points = geom.box(length=bridge_w, width=3 * bridge_w)
 
             block_len = 2 * radius * ring_shape + block_w / 2
 
@@ -346,12 +446,21 @@ class AIMPhotonicChip:
                                                                block_len + tether_w / 2)
             nd.Polygon(points=bridge_points, layer='FNAM').put(block_l / 2 - bridge_l / 2,
                                                                block_len + tether_w + bridge_w / 2)
+            if etch_hole:
+                nd.Polygon(points=bridge_points, layer='FNAM').put(block_l / 2 - bridge_l / 2,
+                                                                   block_len + tether_w + bridge_w / 2 + 2 * bridge_w)
+                for idx in range(int(bridge_l // bridge_w / 2)):
+                    offset = idx * 2 * bridge_w
+                    nd.Polygon(points=vert_bridge_points, layer='FNAM').put(block_l / 2 - bridge_l / 2 + offset,
+                                                                            block_len + tether_w + 1.5 * bridge_w)
+                nd.Polygon(points=vert_bridge_points, layer='FNAM').put(block_l / 2 - bridge_l / 2 + bridge_w + offset,
+                                                                        block_len + tether_w + 1.5 * bridge_w)
 
         return microbridge_ps
 
     def static_ps_simple(self ,w1: float, w2: float, offset1: float, offset2: float, length: float = 90,
                     length_taper: float = 5):
-        ''' 
+        '''
         added by Nate to lay down static phase shifters 05/01/2020 1:30am
         '''
         with nd.Cell(name='static_ps_simple') as static_ps_simple:
@@ -380,7 +489,7 @@ class AIMPhotonicChip:
 
     def static_ps_3(self , offset1: float = 0, offset2: float = 0, length: float = 90,
                     length_taper: float = 5):
-        ''' 
+        '''
         added by Nate to lay down static phase shifters 05/01/2020 1:30am
         '''
         with nd.Cell(name='static_ps_simple') as static_ps_simple:
@@ -439,11 +548,9 @@ class AIMPhotonicChip:
 
         return(static_ps_simple)
 
-
     def comb_drive_ps(self, cblock_dim: Tuple[float, float], teeth_ys: List[float], big_spring_ys: List[float],
                       anchor_spring_ys: List[float], n_teeth: int, teeth_vert_sep: float,
                       gnd_attachment_dim: Tuple[float, float], ps_dim: Tuple[float, float],
-                      # ps_ring_connector_dim: Tuple[float, float], connector_dist
                       anchor_l: float = 6, big_spring_dim: Tuple[float, float] = (65, 2),
                       anchor_spring_dim: Tuple[float, float] = (15, 9.4),
                       pad_l: float = 100, anchor_dim: Tuple[float, float] = (10, 5),
@@ -451,8 +558,6 @@ class AIMPhotonicChip:
                       tooth_pitch: float = 0.6, radius: float = 10):
 
         with nd.Cell(name='comb_drive') as comb_drive_ps:
-            nd.add_xsection('xs_si')
-            nd.add_layer2xsection(xsection='xs_si', layer='SEAM')
             ic = nd.interconnects.Interconnect(xs='xs_si', radius=radius, width=spring_w)
 
             cblock = nd.Polygon(geom.box(length=cblock_dim[0], width=cblock_dim[1]), layer='SEAM')
@@ -503,7 +608,8 @@ class AIMPhotonicChip:
                                                                           teeth_vert_sep)
                 ic.strt(width=gnd_attachment_dim[1],
                         length=gnd_attachment_dim[0]).put(x - n_teeth * tooth_pitch,
-                                                          y + cy - 2 * tooth_dim[1] - teeth_vert_sep - spring_edge_w / 2, 0)
+                                                          y + cy - 2 * tooth_dim[
+                                                              1] - teeth_vert_sep - spring_edge_w / 2, 0)
                 if jj == 0:
                     pad.put()
                 ic.strt(tooth_pitch * n_teeth + tooth_dim[0], width=spring_edge_w).put(
@@ -518,7 +624,8 @@ class AIMPhotonicChip:
                                                                          teeth_vert_sep)
                 ic.strt(width=gnd_attachment_dim[1],
                         length=-gnd_attachment_dim[0]).put(x + n_teeth * tooth_pitch,
-                                                           y + cy - 2 * tooth_dim[1] - teeth_vert_sep - spring_edge_w / 2, 0)
+                                                           y + cy - 2 * tooth_dim[
+                                                               1] - teeth_vert_sep - spring_edge_w / 2, 0)
                 if jj == 0:
                     pad.put(nd.cp.x(), nd.cp.y(), 180)
 
@@ -528,10 +635,26 @@ class AIMPhotonicChip:
             for y in big_spring_ys:
                 soft_spring(big_spring_dim, y + cy)
 
-
-
         return comb_drive_ps
 
+    def ring_resonator(self, radius: float, gap_w: float, interaction_l: float,
+                       interaction_angle: float, racetrack_l: float = 0):
+        y_offset = self.waveguide_w + gap_w
+        with nd.Cell(name=f'ring_resonator_{radius}_{racetrack_l}') as ring_resonator:
+            ic = nd.interconnects.Interconnect(xs='xs_si', radius=radius, width=self.waveguide_w)
+            ic.bend(radius=radius, angle=interaction_angle).put(0, 0, 0)
+            ic.bend(radius=radius, angle=-interaction_angle).put()
+            nd.Pin('c0').put()
+            x, y = nd.cp.x(), nd.cp.y()
+            ic.strt(racetrack_l).put(x - racetrack_l / 2, y + y_offset)
+            ic.bend(radius=radius, angle=180).put()
+            ic.strt(racetrack_l).put()
+            ic.bend(radius=radius, angle=180).put()
+            ic.strt(length=interaction_l).put(x, y, 0)
+            ic.bend(radius=radius, angle=-interaction_angle).put()
+            ic.bend(radius=radius, angle=interaction_angle).put()
+            nd.Pin('b0').put()
+        return ring_resonator
 
 def coupler_path(ic: nd.interconnects.Interconnect, angle: float, interaction_l: float, radius: float = 35):
     input_waveguide = ic.bend(radius=radius, angle=angle).put()
